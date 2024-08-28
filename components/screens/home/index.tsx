2a70bafb3aae4b4ab8ac310a24ccb4d5690610f5
ViniciusMendesp <vmp_tech@outlook.com>
2024-08-28 17:01:06 -0300

    first commit

---
+++ components/screens/home/index.tsx
@@ -0,0 +1,26 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log("Adicionar participante");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
